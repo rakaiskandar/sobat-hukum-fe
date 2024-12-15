b18f5e60807022f4d87f3445dcbcbb3597e30845
“EllyazarS” <ellyazarswastiko@gmail.com>
2024-12-15 23:46:54 +0700

    about-ourlawyer

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,12 @@ import LandingPageLayout from "@/components/layout/LandingPageLayout";
 export default function About() {
     return (
         <LandingPageLayout>
-            About
+            <main class="container mx-auto py-16 text-center">
+                <h1 class="text-4xl font-bold mb-6">Tentang Kami</h1>
+                <p class="text-gray-700 mb-12 max-w-3xl mx-auto">
+                "Sobat Hukum" adalah platform berbasis web yang dirancang untuk memberikan layanan bantuan hukum kepada masyarakat. Sistem ini memungkinkan klien untuk mengajukan kasus hukum, pengacara untuk menangani kasus, dan admin untuk mengelola aktivitas dan verifikasi. Sistem juga mendukung komunikasi real-time antara klien dan pengacara serta memungkinkan pengajuan kasus secara anonim.
+                </p>
+            </main>
         </LandingPageLayout>
     );
 };
--- src/app/our-lawyer/page.tsx
+++ src/app/our-lawyer/page.tsx
@@ -3,7 +3,57 @@ import LandingPageLayout from "@/components/layout/LandingPageLayout";
 export default function OurLawyer() {
     return (
         <LandingPageLayout>
-            Our Lawyer
+        <h1 class="py-6 text-4xl font-bold items-start px-12 mb-6">Lawyer Kami</h1>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 px-6">
+            <div class="bg-white border border-gray-200 p-4 rounded-lg flex items-center gap-4">
+            <img src="reshot-icon-profile-QX6KDSLJC5.svg" alt="Lawyer 1" class="rounded-full w-16 h-16">
+                <div>
+                    <h3 class="text-xl font-bold">Lawyer 1</h3>
+                    <p class="text-gray-700">lawyer1@gmail.com</p>
+                </div>
+            </div>
+
+        <div class="bg-white border border-gray-200 p-4 rounded-lg flex items-center gap-4">
+            <img src="reshot-icon-profile-QX6KDSLJC5.svg" alt="Lawyer 2" class="rounded-full w-16 h-16">
+            <div>
+                <h3 class="text-xl font-bold">Lawyer 2</h3>
+                <p class="text-gray-700">lawyer2@gmail.com</p>
+            </div>
+        </div>
+
+        <div class="bg-white border border-gray-200 p-4 rounded-lg flex items-center gap-4">
+            <img src="reshot-icon-female-profile-WYM2X8PL7U.svg" alt="Lawyer 3" class="rounded-full w-16 h-16">
+            <div>
+                <h3 class="text-xl font-bold">Lawyer 3</h3>
+                <p class="text-gray-700">lawyer3@gmail.com</p>
+            </div>
+        </div>
+
+        <div class="bg-white border border-gray-200 p-4 rounded-lg flex items-center gap-4">
+            <img src="reshot-icon-profile-QX6KDSLJC5.svg" alt="Lawyer 4" class="rounded-full w-16 h-16">
+            <div>
+                <h3 class="text-xl font-bold">Lawyer 4</h3>
+                <p class="text-gray-700">lawyer4@gmail.com</p>
+            </div>
+        </div>
+
+        <div class="bg-white border border-gray-200 p-4 rounded-lg flex items-center gap-4">
+            <img src="reshot-icon-female-profile-WYM2X8PL7U.svg" alt="Lawyer 5" class="rounded-full w-16 h-16">
+            <div>
+                <h3 class="text-xl font-bold">Lawyer 5</h3>
+                <p class="text-gray-700">lawyer5@gmail.com</p>
+            </div>
+        </div>
+
+        <div class="bg-white border border-gray-200 p-4 rounded-lg flex items-center gap-4">
+            <img src="reshot-icon-profile-QX6KDSLJC5.svg" alt="Lawyer 6" class="rounded-full w-16 h-16">
+            <div>
+                <h3 class="text-xl font-bold">Lawyer 6</h3>
+                <p class="text-gray-700">lawyer6@gmail.com</p>
+            </div>
+        </div>
+    </div>
         </LandingPageLayout>
     )
 };
